7c9cf6cf16265eac5c4906cc20d8c5c905297e40
jeffery71 <jefferynthu@gmail.com>
2020-10-16 18:51:49 +0800

    fix

--- smf_detect.py
+++ smf_detect.py
@@ -329,7 +329,10 @@ class OpenStackAPI():
         get_instance_list_response = requests.get(list_instance_url, headers=headers)
         print("Get OpenStack instance list status: " + str(get_instance_list_response.status_code))
         get_instance_list_result = get_instance_list_response.json()
-        print(get_instance_list_result)
+        try:
+            print(get_instance_list_result)
+        except Exception as e:
+            print(f'An Error occurred: {e}')
         print('check1')
         
         instance_list = get_instance_list_result['servers']
