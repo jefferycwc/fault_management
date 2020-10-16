203faeaeb03e6c10f483d69f412a6ef1a37d8b79
jeffery71 <jefferynthu@gmail.com>
2020-10-16 19:08:01 +0800

    fix

--- smf_detect.py
+++ smf_detect.py
@@ -318,7 +318,7 @@ class OpenStackAPI():
         get_instance_list_response = requests.get(list_instance_url, headers=headers)
         print("Get OpenStack instance list status: " + str(get_instance_list_response.status_code))
         get_instance_list_result = get_instance_list_response.json()
-        print('check1')
+        #print('check1')
         #print(get_instance_list_result)
         return get_instance_list_result
     
