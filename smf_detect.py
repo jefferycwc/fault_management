d83eb51e85073d84b662d141b0816cdb70e33dd8
jeffery71 <jefferynthu@gmail.com>
2020-10-16 17:49:41 +0800

    add openstack api

--- smf_detect.py
+++ smf_detect.py
@@ -242,18 +242,94 @@ class TackerAPI():
         print(get_ns_list_result)
         #print(text)
         return get_ns_list_result
+        
+class OpenStackAPI():
+    def __init__(self):
+        #super().__init__()
+        self.OPENSTACK_IP = OPENSTACK_IP
+        self.OS_AUTH_URL = OS_AUTH_URL
+        self.OS_USER_DOMAIN_NAME = OS_USER_DOMAIN_NAME
+        self.OS_USERNAME = OS_USERNAME
+        self.OS_PASSWORD = OS_PASSWORD
+        self.OS_PROJECT_DOMAIN_NAME = OS_PROJECT_DOMAIN_NAME
+        self.OS_PROJECT_NAME = OS_PROJECT_NAME
+        self.ary_data = []
+        self.nsd_id = ''
+        self.nsd_name = ''
+        self.get_token_result = ''
+        self.project_id = ''
 
+    def get_token(self):
+        # print("\nGet token:")
+        self.get_token_result = ''
+        get_token_url = self.OS_AUTH_URL + '/v3/auth/tokens'
+        get_token_body = {
+            'auth': {
+                'identity': {
+                    'methods': [
+                        'password'
+                    ],
+                    'password': {
+                        'user': {
+                            'domain': {
+                                'name': self.OS_USER_DOMAIN_NAME
+                            },
+                            'name': self.OS_USERNAME,
+                            'password': self.OS_PASSWORD
+                        }
+                    }
+                },
+                'scope': {
+                    'project': {
+                        'domain': {
+                            'name': self.OS_PROJECT_DOMAIN_NAME
+                        },
+                        'name': self.OS_PROJECT_NAME
+                    }
+                }
+            }
+        }
+        get_token_response = requests.post(get_token_url, data=json.dumps(get_token_body))
+        #print("Get OpenStack token status: " + str(get_token_response.status_code))
+        self.get_token_result = get_token_response.headers['X-Subject-Token']
+        return self.get_token_result
 
-
-
+    def get_project_id(self, project_name):
+        # print("\nGet Project ID:")
+        self.project_id = ''
+        get_project_list_url = self.OS_AUTH_URL + '/v3/projects'
+        token = self.get_token()
+        headers = {'X-Auth-Token': token}
+        get_project_list_response = requests.get(get_project_list_url, headers=headers)
+        print("Get OpenStack project list status: " + str(get_project_list_response.status_code))
+        get_project_list_result = get_project_list_response.json()['projects']
+        #print(get_project_list_result)
+        for project in get_project_list_result:
+            if project['name'] == project_name:
+                self.project_id = project['id']
+            pass
+        print("Project ID:" + self.project_id)
+        return self.project_id
+    
+    def list_instance(self):
+        list_instance_url = 'http://' + self.OPENSTACK_IP + ':9696/v2.0/servers'
+        token = self.get_token()
+        headers = {'X-Auth-Token': token}
+        get_instance_list_response = requests.get(get_port_url, headers=headers)
+        print("Get OpenStack instance status: " + str(get_instance_list_response.status_code))
+        get_instance_list_result = get_instance_list_response.json()
+        print(get_instance_list_result)
+        return get_instance_list_result
     
 
 if __name__ == '__main__':
     print('start')
-    test = TackerAPI()
+    #test = TackerAPI()
     #test.smf_detect()
-    while 1:
-        test.smf_detect()
+    #while 1:
+    #    test.smf_detect()
+    test = OpenStackAPI()
+    test.list_instance()
     
 
 
