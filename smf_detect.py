2b8e12fab12d191fea8042e6c110a6e81fe9098a
jeffery71 <jefferynthu@gmail.com>
2020-10-16 18:37:07 +0800

    fix

--- smf_detect.py
+++ smf_detect.py
@@ -317,8 +317,8 @@ class OpenStackAPI():
         headers = {'X-Auth-Token': token}
         get_instance_list_response = requests.get(list_instance_url, headers=headers)
         print("Get OpenStack instance list status: " + str(get_instance_list_response.status_code))
-        get_instance_list_result = get_instance_list_response.json()['servers']
-        print(get_instance_list_result)
+        get_instance_list_result = get_instance_list_response.json()
+        #print(get_instance_list_result)
         return get_instance_list_result
     
     def get_instance_id(self,ins_name):
