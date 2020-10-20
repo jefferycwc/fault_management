5d23d033a2b5294d6423eeb357111c8cd85266bf
jeffery71 <jefferynthu@gmail.com>
2020-10-20 16:19:36 +0800

    add em.py

--- em.py
+++ em.py
@@ -3,7 +3,7 @@ import os
 def worker(arg):
     os.system(arg)
 if __name__ == '__main__':
-    files = ["./nrf_detect.py","./amf_detect.py","./smf_detect.py"]
+    files = ["python nrf_detect.py","python amf_detect.py","python smf_detect.py"]
     for i in files:
         p = multiprocessing.Process(target=worker, args=(i,))
         p.start()
