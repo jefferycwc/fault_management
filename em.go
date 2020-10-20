94a518ce5b53beeca48c6923dd1b2ed3b1e4c1b4
jeffery71 <jefferynthu@gmail.com>
2020-10-20 14:55:27 +0800

    add em.go

---
+++ em.go
@@ -0,0 +1,19 @@
+var wg sync.WaitGroup
+func main(){
+	wg.Add(1)
+	go nrf_detect()
+	wg.Add(1)
+	go amf_detct()
+	wg.Add(1)
+	go smf_detct()
+}
+
+func nrf_detect(){
+	exec.Command("python nrf_detect.py")
+}
+func amf_detect(){
+	exec.Command("python amf_detect.py")
+}
+func smf_detect(){
+	exec.Command("python smf_detect.py")
+}
--- em.py
+++
@@ -1,5 +0,0 @@
-import os
-import smf_detect
-os.system("sudo python nrf_detect.py")
-os.system("sudo python amf_detect.py")
-os.system("sudo python smf_detect.py")
