14955386db0970005ca8d0047128f4a91b63e9b0
jeffery71 <jefferynthu@gmail.com>
2020-10-28 16:28:57 +0800

    fix

--- vnf_start.py
+++ vnf_start.py
@@ -12,41 +12,41 @@ class VNF_Start():
         self.nssf_instance_ip='172.24.4.107'
         self.ausf_instance_ip='172.24.4.108'
     
-    def upf_start():
+    def upf_start(self):
         cmds = ['cd /home/ubuntu/stage3/gtp5g\n','sudo make install\n','cd /home/ubuntu/stage3/src/upf\n','mkdir build\n','cd build\n','cmake ..\n','make -j`nproc`\n','sudo nohup ./bin/free5gc-upfd\n','exit\n']
-        ssh_jump(upf_instance_ip,cmds)
+        ssh_jump(self.upf_instance_ip,cmds)
 
-    def nrf_start():
+    def nrf_start(self):
         cmds = ['cd stage3\n','sudo nohup ./bin/nrf\n','exit\n']
-        ssh_jump(nrf_instance_ip,cmds)
+        ssh_jump(self.nrf_instance_ip,cmds)
 
-    def amf_start():
+    def amf_start(self):
         cmds = ['cd stage3\n','sudo nohup ./bin/amf\n','exit\n']
-        ssh_jump(amf_instance_ip,cmds)
+        ssh_jump(self.amf_instance_ip,cmds)
 
-    def smf_start():
+    def smf_start(self):
         cmds = ['cd stage3\n','sudo nohup ./bin/smf\n','exit\n']
-        ssh_jump(smf_instance_ip,cmds)
+        ssh_jump(self.smf_instance_ip,cmds)
 
-    def udr_start():
+    def udr_start(self):
         cmds = ['cd stage3\n','sudo nohup ./bin/udr\n','exit\n']
-        ssh_jump(udr_instance_ip,cmds)
+        ssh_jump(self.udr_instance_ip,cmds)
 
-    def pcf_start():
+    def pcf_start(self):
         cmds = ['cd stage3\n','sudo nohup ./bin/pcf\n','exit\n']
-        ssh_jump(pcf_instance_ip,cmds)
+        ssh_jump(self.pcf_instance_ip,cmds)
 
-    def udm_start():
+    def udm_start(self):
         cmds = ['cd stage3\n','sudo nohup ./bin/udm\n','exit\n']
-        ssh_jump(udm_instance_ip,cmds)
+        ssh_jump(self.udm_instance_ip,cmds)
 
-    def nssf_start():
+    def nssf_start(self):
         cmds = ['cd stage3\n','sudo nohup ./bin/nssf\n','exit\n']
-        ssh_jump(udr_instance_ip,cmds)
+        ssh_jump(self.nssf_instance_ip,cmds)
 
-    def ausf_start():
+    def ausf_start(self):
         cmds = ['cd stage3\n','sudo nohup ./bin/ausf\n','exit\n']
-        ssh_jump(ausf_instance_ip,cmds)
+        ssh_jump(self.ausf_instance_ip,cmds)
 
     
 if __name__ == '__main__':
