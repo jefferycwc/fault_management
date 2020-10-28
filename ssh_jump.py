e54259d6ae6675a95cb032030881e7e2b1cdb507
jeffery71 <jefferynthu@gmail.com>
2020-10-28 13:54:33 +0800

    fix

---
+++ ssh_jump.py
@@ -0,0 +1,29 @@
+import os
+import paramiko
+
+key=paramiko.RSAKey.from_private_key_file('./free5gc.key')
+
+jumpbox_addr = '192.168.1.77'
+#jumpbox_private_addr = '10.0.5.10'
+target_addr = '172.24.4.104'
+
+jumpbox=paramiko.SSHClient()
+jumpbox.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+jumpbox.connect(jumpbox_addr, username='openstack', password='0000')
+
+jumpbox_transport = jumpbox.get_transport()
+src_addr = (jumpbox_addr, 22)
+dest_addr = (target_addr, 22)
+jumpbox_channel = jumpbox_transport.open_channel("direct-tcpip", dest_addr, src_addr)
+
+target=paramiko.SSHClient()
+target.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+target.connect(target_addr, username='ubuntu', pkey=key, sock=jumpbox_channel)
+
+stdin, stdout, stderr = target.exec_command("ifconfig")
+for line in stdout.read().split(b'\n'):
+  print(str(line))
+
+target.close()
+jumpbox.close()
+
