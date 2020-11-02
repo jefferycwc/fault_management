78258396b1b91b0009c3aaef474c928643f4dcd4
jeffery71 <jefferynthu@gmail.com>
2020-11-02 17:28:21 +0800

    fix

--- em.py
+++ em.py
@@ -3,16 +3,22 @@ import os
 import signal
 import sys
 def worker(arg):
-    signal.signal(signal.SIGINT, signal_handler)
     os.system(arg)
-    
-def signal_handler(signal, frame):
-    print 'You pressed Ctrl+C!'
-    # for p in jobs:
-    #     p.terminate()
-    sys.exit(0)
-if __name__ == '__main__':
+
+'''if __name__ == '__main__':
     files = ["python mongo_detect.py","python upf_detect.py","python nrf_detect.py","python amf_detect.py","python smf_detect.py","python udr_detect.py","python pcf_detect.py","python udm_detect.py","python nssf_detect.py","python ausf_detect.py"]
     for i in files:
         p = multiprocessing.Process(target=worker, args=(i,))
-        p.start()
+        p.start()'''
+if __name__ == "__main__":
+    original_sigint_handler = signal.signal(signal.SIGINT, signal.SIG_IGN)
+    pool =Pool(10)
+    cmds = ["python mongo_detect.py","python upf_detect.py","python nrf_detect.py","python amf_detect.py","python smf_detect.py","python udr_detect.py","python pcf_detect.py","python udm_detect.py","python nssf_detect.py","python ausf_detect.py"]
+    signal.signal(signal.SIGINT, original_sigint_handler)
+    try:
+        pool.map_async(worker,cmds)
+    except KeyboardInterrupt:
+        pool.terminate()
+    else:
+        pool.close()
+    pool.join()
