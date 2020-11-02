ba55ed88a76b3d4a25f85adfe67da648a89d1c60
jeffery71 <jefferynthu@gmail.com>
2020-11-02 17:09:59 +0800

    fix

--- em.py
+++ em.py
@@ -1,7 +1,16 @@
 import multiprocessing
 import os
+import signal
+import sys
 def worker(arg):
+    signal.signal(signal.SIGINT, signal_handler)
     os.system(arg)
+    
+def signal_handler(signal, frame):
+    print 'You pressed Ctrl+C!'
+    # for p in jobs:
+    #     p.terminate()
+    sys.exit(0)
 if __name__ == '__main__':
     files = ["python mongo_detect.py","python upf_detect.py","python nrf_detect.py","python amf_detect.py","python smf_detect.py","python udr_detect.py","python pcf_detect.py","python udm_detect.py","python nssf_detect.py","python ausf_detect.py"]
     for i in files:
