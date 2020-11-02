37aadd9390a7c658411832d4fcbd1b989ca9dbdc
jefferycwc <jefferynthu@gmail.com>
2020-11-02 22:29:11 +0800

    fix

--- em.py
+++ em.py
@@ -15,10 +15,10 @@ def worker(arg):
         p = multiprocessing.Process(target=worker, args=(i,))
         p.start()'''
 if __name__ == "__main__":
-    #original_sigint_handler = signal.signal(signal.SIGINT, signal.SIG_IGN)
+    original_sigint_handler = signal.signal(signal.SIGINT, signal.SIG_IGN)
     pool =Pool(10)
     cmds = ["python mongo_detect.py","python upf_detect.py","python nrf_detect.py","python amf_detect.py","python smf_detect.py","python udr_detect.py","python pcf_detect.py","python udm_detect.py","python nssf_detect.py","python ausf_detect.py"]
-    #signal.signal(signal.SIGINT, original_sigint_handler)
+    signal.signal(signal.SIGINT, original_sigint_handler)
     try:
         result=pool.map_async(worker,cmds).get(9999)
     except KeyboardInterrupt:
