7968a85db5002e0075bb9cb0adfa8d6367b75894
jeffery71 <jefferynthu@gmail.com>
2020-11-02 15:16:38 +0800

    fix

--- em.py
+++ em.py
@@ -3,7 +3,7 @@ import os
 def worker(arg):
     os.system(arg)
 if __name__ == '__main__':
-    files = ["python mongo_detect.py","python nrf_detect.py","python amf_detect.py","python smf_detect.py","python udr_detect.py","python pcf_detect.py","python udm_detect.py","python nssf_detect.py","python ausf_detect.py"]
+    files = ["python mongo_detect.py","python upf_detect.py","python nrf_detect.py","python amf_detect.py","python smf_detect.py","python udr_detect.py","python pcf_detect.py","python udm_detect.py","python nssf_detect.py","python ausf_detect.py"]
     for i in files:
         p = multiprocessing.Process(target=worker, args=(i,))
         p.start()
