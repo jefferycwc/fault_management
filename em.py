09c9dcc54648603f4aeb12271ceac0f4aaa56b47
jeffery71 <jefferynthu@gmail.com>
2020-11-02 17:51:42 +0800

    fix

--- em.py
+++ em.py
@@ -4,7 +4,10 @@ import os
 import signal
 import sys
 def worker(arg):
-    os.system(arg)
+    try:
+        os.system(arg)
+    except KeyboardInterrupt:
+        print("Caught KeyboardInterrupt, terminating worker")
 
 '''if __name__ == '__main__':
     files = ["python mongo_detect.py","python upf_detect.py","python nrf_detect.py","python amf_detect.py","python smf_detect.py","python udr_detect.py","python pcf_detect.py","python udm_detect.py","python nssf_detect.py","python ausf_detect.py"]
