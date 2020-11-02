1b6da3e831a7121d40ea222fbe4217690e98c068
jefferycwc <jefferynthu@gmail.com>
2020-11-02 22:36:59 +0800

    fix

--- em.py
+++ em.py
@@ -9,12 +9,12 @@ def worker(arg):
     except KeyboardInterrupt:
         print("Caught KeyboardInterrupt, terminating worker")
 
-'''if __name__ == '__main__':
+if __name__ == '__main__':
     files = ["python mongo_detect.py","python upf_detect.py","python nrf_detect.py","python amf_detect.py","python smf_detect.py","python udr_detect.py","python pcf_detect.py","python udm_detect.py","python nssf_detect.py","python ausf_detect.py"]
     for i in files:
         p = multiprocessing.Process(target=worker, args=(i,))
-        p.start()'''
-if __name__ == "__main__":
+        p.start()
+'''if __name__ == "__main__":
     original_sigint_handler = signal.signal(signal.SIGINT, signal.SIG_IGN)
     pool =Pool(10)
     cmds = ["python mongo_detect.py","python upf_detect.py","python nrf_detect.py","python amf_detect.py","python smf_detect.py","python udr_detect.py","python pcf_detect.py","python udm_detect.py","python nssf_detect.py","python ausf_detect.py"]
@@ -27,4 +27,4 @@ if __name__ == "__main__":
     else:
         print("Normal termination")
         pool.close()
-    pool.join()
+    pool.join()'''
