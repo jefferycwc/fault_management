3b8e2662211c630b2a9982a2af776416a61c4421
jeffery71 <jefferynthu@gmail.com>
2020-11-03 15:32:43 +0800

    fix

--- em.py
+++ em.py
@@ -4,10 +4,6 @@ import os
 import signal
 import sys
 def worker(arg):
-    '''try:
-        os.system(arg)
-    except KeyboardInterrupt:
-        print("Caught KeyboardInterrupt, terminating worker")'''
     os.system(arg)
 
 def kill_process():
@@ -19,11 +15,7 @@ def kill_process():
             pid = fields[0]
             print(pid)
             os.kill(int(pid), signal.SIGKILL)     
-'''if __name__ == '__main__':
-    files = ["python mongo_detect.py","python upf_detect.py","python nrf_detect.py","python amf_detect.py","python smf_detect.py","python udr_detect.py","python pcf_detect.py","python udm_detect.py","python nssf_detect.py","python ausf_detect.py"]
-    for i in files:
-        p = multiprocessing.Process(target=worker, args=(i,))
-        p.start()'''
+            
 if __name__ == "__main__":
     original_sigint_handler = signal.signal(signal.SIGINT, signal.SIG_IGN)
     pool =Pool(10)
