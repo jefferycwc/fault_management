178fa7e662c1026f6afbea6f769a2075456a710b
jeffery71 <jefferynthu@gmail.com>
2020-11-04 14:11:04 +0800

    fix

--- master_node.py
+++ master_node.py
@@ -1,6 +1,8 @@
-import flask
+from flask import Flask
+from flask import render_template
+from flask import request
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 app.config["DEBUG"] = True
 
 
@@ -8,7 +10,8 @@ app.config["DEBUG"] = True
 def home():
     return "<h1>Hello Flask!</h1>"
 
-@app.route('/name', methods=['GET'])
-def name():
-    return "<h1>jeffery!</h1>"
+@app.route('/healvnf', methods=['GET'])
+def healvnf():
+    id = request.args.get('id')
+    return "<h1>heal vnf successfully!</h1>"
 app.run(host='192.168.1.219',port=5010)
