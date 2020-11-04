5f1dd11da252e18b95af81c0121d4bba2a28956c
jeffery71 <jefferynthu@gmail.com>
2020-11-04 17:48:00 +0800

    fix

--- HealVnfRequest.py
+++ HealVnfRequest.py
@@ -6,5 +6,6 @@ def SendHealVnfRequest(id,status,name):
         'name' : name
     }
     url = 'http://192.168.1.219:5010/healvnf'
+    print('Send HealVnfRequest')
     response = requests.post(url,json=body)
-    print(response.text)
+    print('Receive HealVnfResponse, result is {}'.format(response.text))
