0776e06d79af0b14692e2a86a7103a315436258b
jeffery71 <jefferynthu@gmail.com>
2020-11-04 13:55:19 +0800

    fix

--- master_node.py
+++ master_node.py
@@ -8,5 +8,6 @@ app.config["DEBUG"] = True
 def home():
     return "<h1>Hello Flask!</h1>"
 
-
+@app.route('/name', methods=['GET'])
+    return "<h1>jeffery!</h1>"
 app.run(host='192.168.1.219',port=5010)
