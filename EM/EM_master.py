fd8793708f84682d67a7a194b569385bb4183734
jeffery71 <jefferynthu@gmail.com>
2020-11-05 14:53:49 +0800

    fix

--- EM/EM_master.py
+++ EM/EM_master.py
@@ -7,7 +7,7 @@ def worker(arg):
     os.system(arg)
 
 def kill_process():
-    process_name = ['mongo_detect.py','upf_detect.py','nrf_detect.py','amf_detect.py','smf_detect.py','udr_detect.py','pcf_detect.py','udm_detect.py','nssf_detect.py','ausf_detect.py','em.py']
+    process_name = ['EM_mongo.py','EM_upf.py','EM_nrf.py','EM_amf.py','EM_smf.py','EM_udr.py','EM_pcf.py','EM_udm.py','EM_nssf.py','EM_ausf.py','EM_master.py']
     for process in process_name:
         print(process)
         for line in os.popen("ps ax | grep " + process + " | grep -v grep"):
@@ -19,7 +19,7 @@ def kill_process():
 if __name__ == "__main__":
     original_sigint_handler = signal.signal(signal.SIGINT, signal.SIG_IGN)
     pool =Pool(10)
-    cmds = ["python mongo_detect.py","python upf_detect.py","python nrf_detect.py","python amf_detect.py","python smf_detect.py","python udr_detect.py","python pcf_detect.py","python udm_detect.py","python nssf_detect.py","python ausf_detect.py"]
+    cmds = ["python EM_mongo.py","python EM_upf.py","python EM_nrf.py","python EM_amf.py","python EM_smf.py","python EM_udr.py","python EM_pcf.py","python EM_udm.py","python EM_nssf.py","python EM_ausf.py"]
     signal.signal(signal.SIGINT, original_sigint_handler)
     try:
         result=pool.map_async(worker,cmds).get(2000)
