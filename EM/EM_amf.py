cc7e1ecae702461d69c8c91d24177562c4d48d74
jeffery71 <jefferynthu@gmail.com>
2020-11-06 15:59:16 +0800

    fix

--- EM/EM_amf.py
+++ EM/EM_amf.py
@@ -4,7 +4,7 @@ from HealVnfRequest import SendHealVnfRequest
 import os
 import sys
 sys.path.append("..")
-from params.openstack_params import OPENSTACK_IP,OS_AUTH_URL,OS_USER_DOMAIN_NAME,OS_USERNAME,OS_PASSWORD,OS_PROJECT_DOMAIN_NAME,OS_PROJECT_NAME
+from .. params.openstack_params import OPENSTACK_IP,OS_AUTH_URL,OS_USER_DOMAIN_NAME,OS_USERNAME,OS_PASSWORD,OS_PROJECT_DOMAIN_NAME,OS_PROJECT_NAME
 from ssh_jump import ssh_jump 
 from function_reset import reset
 
