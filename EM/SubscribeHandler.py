78dc2743463c0836e5e660b4c5ab7f14937217cd
jeffery71 <jefferynthu@gmail.com>
2020-11-06 19:59:56 +0800

    fix

--- EM/SubscribeHandler.py
+++ EM/SubscribeHandler.py
@@ -3,7 +3,7 @@ from HealVnfRequest import SendHealVnfRequest
 def subscriber(channel_name):
     r = redis.Redis(host='192.168.1.219', port=6379, db=0)
     sub = r.pubsub()
-    sub.subscribe(channel_name)
+    sub.subscribe('pcf_channel','amf_channel')
     for message in sub.listen():
         if message['type'] == 'subscribe':
             if message['data'] == 1:
