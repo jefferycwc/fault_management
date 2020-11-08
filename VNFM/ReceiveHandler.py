12d03c49c255bb8a0017b47f8d686a398269b58e
jefferycwc <jefferynthu@gmail.com>
2020-11-08 23:23:50 +0800

    fix

--- VNFM/ReceiveHandler.py
+++ VNFM/ReceiveHandler.py
@@ -35,5 +35,5 @@ def ReceiveHealVnfRequest():
     thread = Thread(target=HealVnfProcessStart, kwargs={'id':id,'cause':cause,'name':name)
     thread.start()
     print('Send HealVnfResponse to EM')
-    return ß
+    return 
 app.run(host='192.168.1.219',port=5010)
