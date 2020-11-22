4d7440f416ff349920e4818e6974e09da5cff568
jeffery71 <jefferynthu@gmail.com>
2020-11-22 22:22:06 +0800

    fix

--- master_node/ReceiveHandler.py
+++ master_node/ReceiveHandler.py
@@ -2,10 +2,12 @@ from flask import Flask,render_template,request,jsonify
 import requests
 import os
 import time
+from oslo_log import log as logging
 from HealVnfHandler import OpenStackAPI
 from PublishHandler import publisher
 from threading import Thread
 import vnf_detect
+LOG = logging.getLogger(__name__)
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
@@ -15,9 +17,10 @@ def AddMonitor():
     data = request.get_json()
     vnf_id = data['id']
     description = data['description']
-    print('description: {}'.format(description))
+    #print('description: {}'.format(description))
     vnf_name = description.split(':')[1]
-    print('vnf name: {}'.format(vnf_name))
+    #print('vnf name: {}'.format(vnf_name))
+    LOG.debug('Start monitoring {}'.format(vnf_name))
     thread = Thread(target=vnf_detect.start, kwargs={'vnf_name':vnf_name,'vnf_id':vnf_id})
     thread.start()
     return 'succesful'
@@ -32,6 +35,7 @@ def ReceiveHealVnfRequest():
     name = data['name']
     def HealVnfProcessStart(vnf_id,instance_id,cause,name):
         time.sleep(2)
+        LOG.debug('Start healing {} vnf'.format(vnf_name))
         publisher(vnf_id,instance_id,cause,name,'notification1')
         new_item = OpenStackAPI()
         if cause == 'paused':
@@ -40,6 +44,7 @@ def ReceiveHealVnfRequest():
             result = new_item.resume(instance_id,name)
         elif cause == 'shutoff':
             result = new_item.reboot(instance_id,name)
+        LOG.debug('Finish healing {} vnf'.format(vnf_name))
         publisher(vnf_id,instance_id,cause,name,'notification2')
     thread = Thread(target=HealVnfProcessStart, kwargs={'vnf_id':vnf_id,'instance_id':instance_id,'cause':cause,'name':name})
     thread.start()
