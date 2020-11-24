a87b4f57076bf8981080468013843a8b95fe124f
jeffery71 <jefferynthu@gmail.com>
2020-11-24 17:58:23 +0800

    fix

--- master_node/vnf_detect.py
+++ master_node/vnf_detect.py
@@ -8,7 +8,7 @@ def findProcessIdByName(processName):
     #Iterate over the all the running process
     for proc in psutil.process_iter():
         try:
-            pinfo = proc.as_dict(attrs=['pid', 'name', 'create_time'])
+            pinfo = proc.as_dict(attrs=['pid', 'name'])
             # Check if process name contains the given name string.
             if processName.lower() in pinfo['name'].lower() :
                 listOfProcessObjects.append(pinfo)
@@ -17,13 +17,13 @@ def findProcessIdByName(processName):
     return listOfProcessObjects
 
 if __name__ == "__main__":
-    listOfProcessIds = findProcessIdByName('sudo nohup ./bin/pcf')
+    listOfProcessIds = findProcessIdByName('./bin/pcf')
     if len(listOfProcessIds) > 0:
         print('Process Exists | PID and other details are')
         for elem in listOfProcessIds:
             processID = elem['pid']
             processName = elem['name']
-            processCreationTime =  time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(elem['create_time']))
-            print((processID ,processName,processCreationTime ))
+            #processCreationTime =  time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(elem['create_time']))
+            print(processID ,processName)
     else :
         print('No Running Process found with given text')
