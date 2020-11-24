d2cb9af71c8913efd3c67ed6d04adac4104114e7
jeffery71 <jefferynthu@gmail.com>
2020-11-24 17:38:07 +0800

    fix

--- master_node/vnf_detect.py
+++ master_node/vnf_detect.py
@@ -1,32 +1,29 @@
-import os
-import paramiko
-import time
-def ssh_jump(target_addr):
-    key=paramiko.RSAKey.from_private_key_file('./free5gc.key')
-
-    jumpbox=paramiko.SSHClient()
-    jumpbox.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    jumpbox.connect('192.168.1.77', username='jeffery', password='jeffery71')
-
-    jumpbox_transport = jumpbox.get_transport()
-    src_addr = ('192.168.1.77', 22)
-    dest_addr = (target_addr, 22)
-    jumpbox_channel = jumpbox_transport.open_channel("direct-tcpip", dest_addr, src_addr)
-
-    target=paramiko.SSHClient()
-    target.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    target.connect(target_addr, username='ubuntu', pkey=key, sock=jumpbox_channel)
-    stdin, stdout, stderr = target.exec_command('ps -aux | grep ./bin/pcf')
-    err = stderr.readline()
-    out = stdout.readline()
-    print(stdout.read())
-    '''ssh = target.invoke_shell()
-    while(1):
-        ssh.send('ps -aux | grep ./bin/pcf')
-        out=ssh.recv(1024)
-        print(out)'''
-    target.close()
-    jumpbox.close()
+import psutil
+def findProcessIdByName(processName):
+    '''
+    Get a list of all the PIDs of a all the running process whose name contains
+    the given string processName
+    '''
+    listOfProcessObjects = []
+    #Iterate over the all the running process
+    for proc in psutil.process_iter():
+       try:
+           pinfo = proc.as_dict(attrs=['pid', 'name', 'create_time'])
+           # Check if process name contains the given name string.
+           if processName.lower() in pinfo['name'].lower() :
+               listOfProcessObjects.append(pinfo)
+       except (psutil.NoSuchProcess, psutil.AccessDenied , psutil.ZombieProcess) :
+           pass
+    return listOfProcessObjects
 
 if __name__ == "__main__":
-    ssh_jump('172.24.4.105')
+    listOfProcessIds = findProcessIdByName('./bin/pcf')
+    if len(listOfProcessIds) > 0:
+    print('Process Exists | PID and other details are')
+    for elem in listOfProcessIds:
+        processID = elem['pid']
+        processName = elem['name']
+        processCreationTime =  time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(elem['create_time']))
+        print((processID ,processName,processCreationTime ))
+    else :
+    print('No Running Process found with given text')
