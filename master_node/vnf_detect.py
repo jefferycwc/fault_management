759f384503b7abc167c7af7c7c0a358f3f01347f
jeffery71 <jefferynthu@gmail.com>
2020-11-24 17:17:24 +0800

    fix

---
+++ master_node/vnf_detect.py
@@ -0,0 +1,28 @@
+import os
+import paramiko
+import time
+def ssh_jump(target_addr):
+    key=paramiko.RSAKey.from_private_key_file('./free5gc.key')
+
+    jumpbox=paramiko.SSHClient()
+    jumpbox.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    jumpbox.connect('192.168.1.77', username='jeffery', password='jeffery71')
+
+    jumpbox_transport = jumpbox.get_transport()
+    src_addr = ('192.168.1.77', 22)
+    dest_addr = (target_addr, 22)
+    jumpbox_channel = jumpbox_transport.open_channel("direct-tcpip", dest_addr, src_addr)
+
+    target=paramiko.SSHClient()
+    target.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    target.connect(target_addr, username='ubuntu', pkey=key, sock=jumpbox_channel)
+    ssh = target.invoke_shell()
+    while(1)
+        ssh.send('ps -aux | grep ./bin/pcf')
+        out=ssh.recv(1024)
+        print(out)
+    target.close()
+    jumpbox.close()
+
+if __name__ == "__main__":
+    ssh_jump('172.24.4.111')
