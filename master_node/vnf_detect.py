9f83afc8c5dcf99f4c4f5e30b6f175cdac66517d
jeffery71 <jefferynthu@gmail.com>
2020-11-24 17:54:03 +0800

    fix

--- master_node/vnf_detect.py
+++ master_node/vnf_detect.py
@@ -8,8 +8,8 @@ def findProcessIdByName(processName):
     #Iterate over the all the running process
     for proc in psutil.process_iter():
         try:
-           pinfo = proc.as_dict(attrs=['pid', 'name', 'create_time'])
-           # Check if process name contains the given name string.
+            pinfo = proc.as_dict(attrs=['pid', 'name', 'create_time'])
+            # Check if process name contains the given name string.
             if processName.lower() in pinfo['name'].lower() :
                 listOfProcessObjects.append(pinfo)
         except (psutil.NoSuchProcess, psutil.AccessDenied , psutil.ZombieProcess) :
