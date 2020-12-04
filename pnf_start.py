3fd33e47263444dc5de2888e8eb782b199bdb17d
jeffery71 <jefferynthu@gmail.com>
2020-12-04 17:40:12 +0800

    fix

--- pnf_start.py
+++ pnf_start.py
@@ -8,7 +8,7 @@ class PnfStart():
         self.target_username = 'jeffery'
         self.target_password = 'jeffery71'
         self.cmds = ['sudo nohup ./all_in_one/src/upf/build/free5gc-upfd\n','sudo nohup ./all_in_one/bin/nrf\n','sudo nohup ./all_in_one/bin/amf\n','sudo nohup ./all_in_one/bin/smf\n',
-        'sudo nohup ./all_in_one/bin/udr\n','sudo nohup ./all_in_one/bin/pcf\n','sudo nohup ./all_in_one/bin/udm\n','sudo nohup ./all_in_one/bin/nssf\n','sudo nohup ./all_in_one/bin/auf\n']
+        'sudo nohup ./all_in_one/bin/udr\n','sudo nohup ./all_in_one/bin/pcf\n','sudo nohup ./all_in_one/bin/udm\n','sudo nohup ./all_in_one/bin/nssf\n','sudo nohup ./all_in_one/bin/auf\n','exit\n']
    
     def start(self):
         connector = RemoteConnect(self.target_addr)
