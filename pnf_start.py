2190dba9f56a60a79565c1ca4099aeef42684fb9
jefferycwc <jefferynthu@gmail.com>
2020-12-06 21:48:53 +0800

    fix

--- pnf_start.py
+++ pnf_start.py
@@ -7,8 +7,7 @@ class PnfStart():
         self.target_addr = '192.168.1.219'
         self.target_username = 'jeffery'
         self.target_password = 'jeffery71'
-        self.cmds = ['sudo nohup ./all_in_one/src/upf/build/bin/free5gc-upfd\n','sudo nohup ./all_in_one/bin/nrf\n','sudo nohup ./all_in_one/bin/amf\n','sudo nohup ./all_in_one/bin/smf\n',
-        'sudo nohup ./all_in_one/bin/udr\n','sudo nohup ./all_in_one/bin/pcf\n','sudo nohup ./all_in_one/bin/udm\n','sudo nohup ./all_in_one/bin/nssf\n','sudo nohup ./all_in_one/bin/auf\n','exit\n']
+        self.cmds = ['sudo nohup ./all_in_one/src/upf/build/bin/free5gc-upfd\n','sudo nohup ./all_in_one/bin/nrf\n','sudo nohup ./all_in_one/bin/amf\n','sudo nohup ./all_in_one/bin/smf\n','sudo nohup ./all_in_one/bin/udr\n','sudo nohup ./all_in_one/bin/pcf\n','sudo nohup ./all_in_one/bin/udm\n','sudo nohup ./all_in_one/bin/nssf\n','sudo nohup ./all_in_one/bin/auf\n','exit\n']
    
     def start(self):
         connector = RemoteConnect(self.target_addr)
