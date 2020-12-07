87aa250921884721ef2e17c3b328f773fce02cb8
jeffery71 <jefferynthu@gmail.com>
2020-12-07 15:43:50 +0800

    fix

--- master_node/function_reset.py
+++ master_node/function_reset.py
@@ -11,49 +11,49 @@ def reset(ip):
     elif ip=='172.24.4.102':
         connector = RemoteConnect(ip)
         print('reset amf')
-        cmds = ['sudo service VnfDetect stop','sudo kill $(pidof ./bin/amf)\n','cd /home/ubuntu/stage3\n','sudo nohup ./bin/amf\n','exit\n']
+        cmds = ['sudo service VnfDetect stop','sudo kill $(pidof ./stage3/bin/amf)\n','cd /home/ubuntu/stage3\n','sudo nohup ./bin/amf\n','exit\n']
         connector.ssh_jump(cmds)
         cmds = ['sudo service VnfDetect restart\n','exit\n']
         connector.ssh_jump(cmds)
     elif ip=='172.24.4.103':
         connector = RemoteConnect(ip)
         print('reset smf')
-        cmds = ['sudo service VnfDetect stop','sudo kill $(pidof ./bin/smf)\n','cd /home/ubuntu/stage3\n','sudo nohup ./bin/smf\n','exit\n']
+        cmds = ['sudo service VnfDetect stop','sudo kill $(pidof ./stage3/bin/smf)\n','cd /home/ubuntu/stage3\n','sudo nohup ./bin/smf\n','exit\n']
         connector.ssh_jump(cmds)
         cmds = ['sudo service VnfDetect restart\n','exit\n']
         connector.ssh_jump(cmds)
     elif ip=='172.24.4.104':
         connector = RemoteConnect(ip)
         print('reset udr')
-        cmds = ['sudo service VnfDetect stop','sudo kill $(pidof ./bin/udr)\n','cd /home/ubuntu/stage3\n','sudo nohup ./bin/udr\n','exit\n']
+        cmds = ['sudo service VnfDetect stop','sudo kill $(pidof ./stage3/bin/udr)\n','cd /home/ubuntu/stage3\n','sudo nohup ./bin/udr\n','exit\n']
         connector.ssh_jump(cmds)
         cmds = ['sudo service VnfDetect restart\n','exit\n']
         connector.ssh_jump(cmds)
     elif ip=='172.24.4.105':
         connector = RemoteConnect(ip)
         print('reset pcf')
-        cmds = ['sudo service VnfDetect stop','sudo kill $(pidof ./bin/pcf)\n','cd /home/ubuntu/stage3\n','sudo nohup ./bin/pcf\n','exit\n']
+        cmds = ['sudo service VnfDetect stop','sudo kill $(pidof ./stage3/bin/pcf)\n','cd /home/ubuntu/stage3\n','sudo nohup ./bin/pcf\n','exit\n']
         connector.ssh_jump(cmds)
         cmds = ['sudo service VnfDetect restart\n','exit\n']
         connector.ssh_jump(cmds)
     elif ip=='172.24.4.106':
         connector = RemoteConnect(ip)
         print('reset udm')
-        cmds = ['sudo service VnfDetect stop','sudo kill $(pidof ./bin/udm)\n','cd /home/ubuntu/stage3\n','sudo nohup ./bin/udm\n','exit\n']
+        cmds = ['sudo service VnfDetect stop','sudo kill $(pidof ./stage3/bin/udm)\n','cd /home/ubuntu/stage3\n','sudo nohup ./bin/udm\n','exit\n']
         connector.ssh_jump(cmds)
         cmds = ['sudo service VnfDetect restart\n','exit\n']
         connector.ssh_jump(cmds)
     elif ip=='172.24.4.107':
         connector = RemoteConnect(ip)
         print('reset nssf')
-        cmds = ['sudo service VnfDetect stop','sudo kill $(pidof ./bin/nssf)\n','cd /home/ubuntu/stage3\n','sudo nohup ./bin/nssf\n','exit\n']
+        cmds = ['sudo service VnfDetect stop','sudo kill $(pidof ./stage3/bin/nssf)\n','cd /home/ubuntu/stage3\n','sudo nohup ./bin/nssf\n','exit\n']
         connector.ssh_jump(cmds)
         cmds = ['sudo service VnfDetect restart\n','exit\n']
         connector.ssh_jump(cmds)
     elif ip=='172.24.4.108':
         connector = RemoteConnect(ip)
         print('reset ausf')
-        cmds = ['sudo service VnfDetect stop','sudo kill $(pidof ./bin/ausf)\n','cd /home/ubuntu/stage3\n','sudo nohup ./bin/ausf\n','exit\n']
+        cmds = ['sudo service VnfDetect stop','sudo kill $(pidof ./stage3/bin/ausf)\n','cd /home/ubuntu/stage3\n','sudo nohup ./bin/ausf\n','exit\n']
         connector.ssh_jump(cmds)
         cmds = ['sudo service VnfDetect restart\n','exit\n']
         connector.ssh_jump(cmds)
