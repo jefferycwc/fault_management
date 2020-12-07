2d071c4b0051eeb4a0a73a4a3d11ee37b473b46e
jeffery71 <jefferynthu@gmail.com>
2020-12-07 14:53:10 +0800

    fix

--- pnf_start.py
+++ pnf_start.py
@@ -12,47 +12,66 @@ class PnfStart():
     def upf_start(self):
         cmds = ['cd /home/jeffery/all_in_one/src/upf/build\n','nohup ./bin/free5gc-upfd\n','exit\n']
         connector = RemoteConnect(self.target_addr)
+        print('Start to activate UPF')
         connector.ssh_direct(cmds,self.target_username,self.target_password)
+        print('Finish to activate UPF')
 
     def nrf_start(self):
         cmds = ['nohup ./all_in_one/bin/nrf\n','exit\n']
         connector = RemoteConnect(self.target_addr)
+        print('Start to activate NRF')
         connector.ssh_direct(cmds,self.target_username,self.target_password)
+        print('Finish to activate NRF')
 
     def amf_start(self):
         cmds = ['nohup ./all_in_one/bin/amf\n','exit\n']
         connector = RemoteConnect(self.target_addr)
+        print('Start to activate AMF')
         connector.ssh_direct(cmds,self.target_username,self.target_password)
+        print('Finish to activate AMF')
 
     def smf_start(self):
         cmds = ['nohup ./all_in_one/bin/smf\n','exit\n']
         connector = RemoteConnect(self.target_addr)
+        print('Start to activate SMF')
         connector.ssh_direct(cmds,self.target_username,self.target_password)
-    
+        print('Finish to activate SMF')
+
     def udr_start(self):
         cmds = ['nohup ./all_in_one/bin/udr\n','exit\n']
         connector = RemoteConnect(self.target_addr)
+        print('Start to activate UDR')
         connector.ssh_direct(cmds,self.target_username,self.target_password)
+        print('Finish to activate UDR')
 
     def pcf_start(self):
         cmds = ['nohup ./all_in_one/bin/pcf\n','exit\n']
         connector = RemoteConnect(self.target_addr)
+        print('Start to activate PCF')
         connector.ssh_direct(cmds,self.target_username,self.target_password)
+        print('Finish to activate PCF')
 
     def udm_start(self):
         cmds = ['nohup ./all_in_one/bin/udm\n','exit\n']
         connector = RemoteConnect(self.target_addr)
+        print('Start to activate UDM')
         connector.ssh_direct(cmds,self.target_username,self.target_password)
+        print('Finish to activate UDM')
 
     def nssf_start(self):
         cmds = ['nohup ./all_in_one/bin/nssf\n','exit\n']
         connector = RemoteConnect(self.target_addr)
+        print('Start to activate NSSF')
         connector.ssh_direct(cmds,self.target_username,self.target_password)
+        print('Finish to activate NSSF')
 
     def ausf_start(self):
         cmds = ['nohup ./all_in_one/bin/ausf\n','exit\n']
         connector = RemoteConnect(self.target_addr)
+        print('Start to activate AUSF')
         connector.ssh_direct(cmds,self.target_username,self.target_password)
+        print('Finish to activate AUSF')
+        
 if __name__ == '__main__':
     test=PnfStart()
     test.upf_start()
