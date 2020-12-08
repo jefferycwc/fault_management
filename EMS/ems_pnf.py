2e262c68b83c7071ae321041dcec9afc0a08edfd
jeffery71 <jefferynthu@gmail.com>
2020-12-08 15:46:01 +0800

    fix

---
+++ EMS/ems_pnf.py
@@ -0,0 +1,21 @@
+import sys
+import paramiko
+def DetectPnf(pnf_name):
+    ssh = paramiko.SSHClient()
+    ssh.connect('192.168.1.219',22,username='jeffery', password='jeffery71')
+    while(1):
+        stdin, stdout, stderr = ssh.exec_command('ps -aux | grep %s' % pnf_name)
+        print(stdout.read().decode())
+
+def kill_process():
+    sys.exit(1)
+
+if __name__ == '__main__':
+    argv = sys.argv[1]
+    original_sigint_handler = signal.signal(signal.SIGINT, signal.SIG_IGN)
+    signal.signal(signal.SIGINT, original_sigint_handler)
+    try:
+        DetectPnf(argv)
+    except KeyboardInterrupt:
+        print("Terminate EM")
+        kill_process()
